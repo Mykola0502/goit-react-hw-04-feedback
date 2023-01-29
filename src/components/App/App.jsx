0b7685d5cc0f768f73bea34f099f4a408a7bf1e8
Mykola Saliuk <haker0502@ukr.net>
2023-01-29 11:46:27 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useReducer, Component } from 'react';
+import { useState } from 'react';
 
 import { Section } from 'components/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions';
@@ -8,21 +8,11 @@ import { Notification } from 'components/Notification';
 import { Container } from './App.styled';
 
 export const App = () => {
-  // const [good, setGood] = useState(0);
-  // const [neutral, setNeutral] = useState(0);
-  // const [bad, setBad] = useState(0);
-
   const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
   });
-
-  // const feedbackReducer = (prevState, name) => {
-  //   return prevState[name] + 1;
-  // };
-
-  // const [state, setFeedback] = useReducer(feedbackReducer, 0);
 
   const leaveFeedback = name => {
     setState(prevState => ({ ...prevState, [name]: prevState[name] + 1 }));
@@ -49,8 +39,6 @@ export const App = () => {
         <FeedbackOptions
           options={options}
           onLeaveFeedback={leaveFeedback}
-          // setCount={setFeedback}
-          // onLeaveFeedback={feedbackReducer}
         ></FeedbackOptions>
       </Section>
       <Section title="Statistics" children>
@@ -68,66 +56,3 @@ export const App = () => {
     </Container>
   );
 };
-
-// import { Component } from 'react';
-
-// import { Section } from 'components/Section';
-// import { FeedbackOptions } from 'components/FeedbackOptions';
-// import { Statistics } from 'components/Statistics';
-// import { Notification } from 'components/Notification';
-
-// import { Container } from './App.styled';
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   leaveFeedback = name => {
-//     this.setState(prevState => ({
-//       [name]: prevState[name] + 1,
-//     }));
-//   };
-
-//   countTotalFeedback = () => {
-//     const values = Object.values(this.state);
-//     let total = 0;
-//     for (const value of values) {
-//       total += value;
-//     }
-//     return total;
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-//   };
-
-//   render() {
-//     const options = Object.keys(this.state);
-
-//     return (
-//       <Container>
-//         <Section title="Please leave feedback" children>
-//           <FeedbackOptions
-//             options={options}
-//             onLeaveFeedback={this.leaveFeedback}
-//           ></FeedbackOptions>
-//         </Section>
-//         <Section title="Statistics" children>
-//           {this.countTotalFeedback() ? (
-//             <Statistics
-//               options={options}
-//               stat={this.state}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </Container>
-//     );
-//   }
-// }
